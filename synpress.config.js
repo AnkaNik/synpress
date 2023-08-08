2ca221f8b740ab2579bf1c50f4f203f704464433
Nikolaieva Anna <anna.nikolaeva@inn4science.com>
2023-08-08 10:01:15 +0200

    upload separate tests

---
+++ synpress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress');
+const synpressPlugins = require('@synthetixio/synpress/plugins');
+ 
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://metamask.github.io/test-dapp/',
+    specPattern: 'tests/e2e/specs',
+    supportFile: 'tests/support/index.js',
+    videosFolder: 'tests/e2e/videos',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    viewportHeight:800,
+    viewportWidth:1280, 
+    video: true,
+    screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      synpressPlugins(on, config);
+      return config
+    },
+  },
+});
